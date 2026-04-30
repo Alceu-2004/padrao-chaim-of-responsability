86f9c2e0f23cf2b0013649b3a81758d10903c17c
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-04-30 11:21:11 -0300

    feat: implementação de pontuação de risco para localização suspeita

--- src/main/handler/LocalizacaoHandler.java
+++ src/main/handler/LocalizacaoHandler.java
@@ -7,7 +7,8 @@ public class LocalizacaoHandler extends AbstractHandler {
     @Override
     public boolean handle(Transacao transacao) {
         if (!transacao.getLocalizacaoUsuario().equals(transacao.getLocalizacaoTransacao())) {
-            System.out.println("Localização suspeita detectada.");
+            System.out.println("Localização suspeita: +40 risco");
+            transacao.adicionarRisco(40);
         }
         return next(transacao);
     }
