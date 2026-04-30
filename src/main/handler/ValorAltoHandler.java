f8bfac30988140ed9df6a02878687000fe597300
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-04-30 11:20:47 -0300

    feat: implementação de pontuação de risco para valor alto

--- src/main/handler/ValorAltoHandler.java
+++ src/main/handler/ValorAltoHandler.java
@@ -7,7 +7,8 @@ public class ValorAltoHandler extends AbstractHandler {
     @Override
     public boolean handle(Transacao transacao) {
         if (transacao.getValor() > 10000) {
-            System.out.println("Transação com valor alto detectada.");
+            System.out.println("Valor alto: +30 risco");
+            transacao.adicionarRisco(30);
         }
         return next(transacao);
     }
