c7bbe0bee380c9d573ce88050d4c4de42b2516a4
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-04-30 11:11:31 -0300

    feat: criação da classe Main com execução inicial do sistema antifraude

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+import model.Transacao;
+import service.AntifraudeService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Transacao transacao = new Transacao(15000, "Brasil", "Rússia", true);
+
+        AntifraudeService service = new AntifraudeService();
+
+        boolean aprovada = service.processar(transacao);
+
+        System.out.println("Transação aprovada? " + aprovada);
+    }
+}
